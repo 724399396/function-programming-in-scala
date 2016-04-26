bd9f80ca21e059f8522ddf00f661f78e6873bed5
liwei <liwei72439936@gmail.com>
2016-04-26 21:32:00 +0800

    update

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -128,12 +128,6 @@ case class State[S,+A](run: S => (A, S)) {
         f(v).run((s2))
       }
     )
-
-  def get: State[S, S] =
-    State((s: S) => (s,s))
-
-  def set(s: S): State[S, Unit] =
-    State(_ => ((), s))
 }
 
 sealed trait Input
@@ -148,22 +142,37 @@ object State {
   def unit[S,A](a: A): State[S,A] =
     State(s => (a, s))
 
-
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
-    def help(input: Input): State[Machine, Unit] = {
-      (machine: Matchine) => ((), (input, machine) match {
-        case (_, Machine(_, 0, _) @ m) =>
-          ((), m)
-        case (Coin, Machine(true, candies, coins)) if candies > 0 =>
-          ((), Machine(false, candies, coins + 1))
-        case (Coin, Machine(locked, candies, coins)) =>
-          ((), Machine(locked, candies, coins + 1))
-        case (Turn, Machine(false, candies, coins)) =>
-          ((), Machine(true, candies-1, conins))
-        case (Turn, Machine(true, _, _) @ m) =>
-          ((),m)
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S,A]], acc: List[A]): (List[A],S) =
+      actions match {
+        case Nil => (acc.reverse,s)
+        case h :: t => h.run(s) match { case (a,s2) => go(s2, t, a :: acc) }
       }
+    State((s: S) => go(s,sas,List()))
+  }
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get1 // Gets the current state and assigns it to `s`.
+    _ <- set1(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  def get1[S]: State[S, S] = State(s => (s, s))
+
+  def set1[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def update = (i: Input) => (s: Machine) =>
+    (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
     }
 
-
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs map ((modify[Machine] _) compose update))
+    s <- get1
+  } yield (s.coins, s.candies)
 }
