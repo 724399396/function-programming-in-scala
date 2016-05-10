543f9272e8a78c466169d62e5ff1ba6bf529709b
liww.li <liww.li@qunar.com>
2016-05-10 18:44:01 +0800

    update

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,11 +1,19 @@
 package fpinscala.parsing
 
 import language.higherKinds
+import scala.util.matching.Regex
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps(p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+  ParserOps[String] = ParserOps(f(a))
+  implicit def regex(r: Regex): Parser[String]
 
-  def many[A](p: Parser[A]): Parser[List[A]]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
+
+  def many[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _) or succeed(List())
 
   def map[A,B](a: Parser[A])(f: A => B): Parser[B]
 
@@ -15,15 +23,44 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   def succeed[A](a: A): Parser[A] =
     string("") map (_ => a)
 
-  implicit def string(s: String): Parser[String]
-  implicit def operators[A](p: Parser[A]) = ParserOps(p)
-  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
-    ParserOps[String] = ParserOps(f(a))
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))(_ :: _)
+
+  def slice[A](p: Parser[A]): Parser[String]
+
+  def product[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)]
+
+  def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C) =
+    map(product(p, p2))(f.tupled)
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
+    List.fill(n)(p).foldRight(succeed(List[A]()))(map2(_, _)(_ :: _))
+
+  def chooseOr[A](left: Boolean, s1: => Parser[A], s2: => Parser[A]): Parser[A] =
+    if (left) s1 else s2
+
+  def flatMap[A,B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  def productViaFlatMap[A,B](p: Parser[A], p2: => Parser[B]): Parser[(A,B)] =
+    for {
+      a <- p
+      b <- p2
+    } yield (a, b)
+
+  def map2ViaFlatMap[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
+    for { a <- p; b <- p2 } yield f(a,b)
+
+  def mapViaFlatMap[A,B](p: Parser[A])(f: A => B): Parser[B] =
+    flatMap(p)(x => succeed(f(x)))
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
     def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def slice: Parser[String] = self.slice(p)
+    def product[B](p2: Parser[B]): Parser[(A,B)] = self.product(p,p2)
+    def **[B](p2: Parser[B]): Parser[(A,B)] = product(p2)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {
