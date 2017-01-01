373136d881c2afd161db1aeb80299d9e0a1139d1
liww.li <liww.li@qunar.com>
2017-01-01 15:19:15 +0800

    finish chapter 6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,32 +30,90 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (v, r) = rng.nextInt
+    (if (v < 0) -(v+1) else v, r)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i.toDouble / (Int.MaxValue + 1).toDouble, r)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i,d), r2)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val (d, r) = double(rng)
+    val (i, r2) = r.nextInt
+    ((d,i), r2)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1,d2,d3), r3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0)
+      (List[Int](), rng)
+    else {
+      val (i, nr) = rng.nextInt
+      val (list, lr) = ints(count - 1)(nr)
+      (i :: list, lr)
+    }
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def doubleViaMap: Rand[Double] =
+    map(nonNegativeInt)(x => x / Int.MaxValue.toDouble + 1)
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    (rng: RNG) => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit[List[A]](Nil))(map2(_,_)(_ :: _))
+
+  def intsViaSequence(count: Int) = sequence(List.fill(count)(int))
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (x,r) = f(rng)
+      g(x)(r)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(i) else nonNegativeLessThan(n)
+    }
+
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(x => unit(f(x)))
+
+  def map2ViaFlatMap[A,B,C](ra: Rand[A])(rb: Rand[B])(f: (A,B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a,b)))
 }
 
 case class State[S,+A](run: S => (A, S)) {
+  import State._
   def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
+    flatMap(a => unit(f(a)))
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
+    flatMap(a => sb.map(b => f(a,b)))
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+    State(s => {
+      val (x, s1) = run(s)
+      f(x).run(s1)
+    })
 }
 
 sealed trait Input
@@ -66,5 +124,24 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+  def sequence[S,A](list: List[State[S,A]]): State[S,List[A]] =
+    list.foldRight(unit[S,List[A]](Nil))(_.map2(_)(_ :: _))
+  def get[S]: State[S,S] = State(s => (s,s))
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    def process(input: Input): State[Machine,Unit] = State(m => {
+      (input, m) match {
+        case (_, Machine(_, 0, _)) => ((),m)
+        case (Coin, Machine(true, candies, coins)) if candies > 0 => ((), Machine(false, candies, coins + 1))
+        case (Turn, Machine(false, candies, coins)) => ((), Machine(true, candies - 1, coins))
+        case _ => ((), m)
+      }
+    })
+    for {
+      _ <- sequence(inputs.map(process))
+      x <- get
+    } yield (x.candies, x.coins)
+  }
 }
