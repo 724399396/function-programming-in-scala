9e3647cea564e464a2ef6153e16a22820fccefb9
liww.li <liww.li@qunar.com>
2017-01-24 13:28:33 +0800

    hangs on chapter 15

--- exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -136,7 +136,16 @@ object SimpleStreamTransducers {
     /*
      * Exercise 5: Implement `|>`. Let the types guide your implementation.
      */
-    def |>[O2](p2: Process[O,O2]): Process[I,O2] = ???
+    def |>[O2](p2: Process[O,O2]): Process[I,O2] = p2 match {
+      case Halt() => Halt()
+      case Emit(h,t) => Emit(h, this|>t)
+      case Await(f) => this match {
+        case Emit(h,t) => t |> f(Some(h))
+        case Halt() => Halt() |> f(None)
+        case Await(g) => Await((i: Option[I]) => g(i) |> p2)
+      }
+
+    }
 
     /*
      * Feed `in` to this `Process`. Uses a tail recursive loop as long
@@ -292,13 +301,23 @@ object SimpleStreamTransducers {
     /*
      * Exercise 1: Implement `take`, `drop`, `takeWhile`, and `dropWhile`.
      */
-    def take[I](n: Int): Process[I,I] = ???
+    def take[I](n: Int): Process[I,I] =
+      if (n <= 0) Halt()
+      else await(i => emit(i, take(n-1)))
 
-    def drop[I](n: Int): Process[I,I] = ???
+    def drop[I](n: Int): Process[I,I] =
+      if (n <= 0) id
+      else await(_ => drop(n-1))
 
-    def takeWhile[I](f: I => Boolean): Process[I,I] = ???
+    def takeWhile[I](f: I => Boolean): Process[I,I] =
+      await(i =>
+        if (f(i)) emit(i, takeWhile(f))
+        else Halt())
 
-    def dropWhile[I](f: I => Boolean): Process[I,I] = ???
+    def dropWhile[I](f: I => Boolean): Process[I,I] =
+      await(i =>
+        if (f(i)) dropWhile(f)
+        else emit(i,id))
 
     /* The identity `Process`, just repeatedly echos its input. */
     def id[I]: Process[I,I] = lift(identity)
@@ -306,7 +325,8 @@ object SimpleStreamTransducers {
     /*
      * Exercise 2: Implement `count`.
      */
-    def count[I]: Process[I,Int] = ???
+    def count[I]: Process[I,Int] =
+      lift((i: I) => 1.0) |> sum |> lift(_.toInt)
 
     /* For comparison, here is an explicit recursive implementation. */
     def count2[I]: Process[I,Int] = {
@@ -318,7 +338,16 @@ object SimpleStreamTransducers {
     /*
      * Exercise 3: Implement `mean`.
      */
-    def mean: Process[Double,Double] = ???
+    def mean: Process[Double,Double] = {
+      def go(n: Int, acc: Double): Process[Double,Double] = {
+        await((d: Double) => {
+          val nAcc = acc+d
+          val nN = n+1
+          emit(nAcc/nN, go(nN, nAcc))
+        })
+      }
+      go(0,0.0)
+    }
 
     def loop[S,I,O](z: S)(f: (I,S) => (O,S)): Process[I,O] =
       await((i: I) => f(i,z) match {
@@ -327,9 +356,11 @@ object SimpleStreamTransducers {
 
     /* Exercise 4: Implement `sum` and `count` in terms of `loop` */
 
-    def sum2: Process[Double,Double] = ???
+    def sum2: Process[Double,Double] =
+      loop(0.0)((x,acc) => (x+acc,x+acc))
 
-    def count3[I]: Process[I,Int] = ???
+    def count3[I]: Process[I,Int] =
+      loop(0)((x,acc) => (acc+1,acc+1))
 
     /*
      * Exercise 7: Can you think of a generic combinator that would
